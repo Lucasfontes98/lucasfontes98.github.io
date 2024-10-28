5386fa3aea289acf6d5b0ee566598508fe41b184
Lucas Fontes <105467544+Lucasfontes98@users.noreply.github.com>
2024-10-28 09:37:27 -0300

    Add files via upload

---
+++ portfol/script/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $("#menu").click(function () {
+      $(this).toggleClass("active");
+      $("header").toggleClass("open");
+      $(".navbar").toggle(); // Alternar visibilidade do menu
+    });
+  
+    // Fecha o menu ao clicar fora
+    $(document).click(function (event) {
+      var target = $(event.target);
+      if (
+        !target.closest("header").length &&
+        !target.closest("#menu").length &&
+        $("header").hasClass("open")
+      ) {
+        $("#menu").removeClass("active");
+        $("header").removeClass("open");
+        $(".navbar").hide();
+      }
+    });
+  });
+  // Animação das barras de progresso
+  setTimeout(() => {
+    $(".habilidades .progress").each(function () {
+      var targetWidth = $(this).data("width"); // Certifique-se de que o atributo data-width está definido
+      $(this).css("width", "0").animate(
+        {
+          width: targetWidth,
+        },
+        2000
+      );
+    });
+  }, 500);
